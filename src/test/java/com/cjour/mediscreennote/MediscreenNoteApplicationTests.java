d363fce762f6f2b7a663bc4bbb7b95284968e8be
Clément <jour.clement@gmail.com>
2021-09-30 08:10:52 +0200

    creating project

---
+++ src/test/java/com/cjour/mediscreennote/MediscreenNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjour.mediscreennote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
